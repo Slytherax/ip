fcaf2d35292843daf80c6978472fb3c1515ecf0a
Laxshan <slytherax11@gmail.com>
2026-08-19 13:00:50 +0000

    Stage 2: Add, List

--- src/main/java/KDB.java
+++ src/main/java/KDB.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class KDB {
     public static void main(String[] args) {
+
+        String[] storage = new String[100];
+
         String banner =
                 "mm   mm   mmmmmm    mmmmmmm\n"
               + "##  ##    ##    ##  ##    ##\n"
@@ -14,23 +17,35 @@ public class KDB {
         System.out.println(banner);
         System.out.println("Hello! I'm KDB.");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("____________________________________________________________\n");
 
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
             String command = scanner.nextLine();
 
-            System.out.println("____________________________________________________________");
+            System.out.println("____________________________________________________________\n");
 
             if (command.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!\n");
+                System.out.println("____________________________________________________________\n");
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < storage.length; i++) {
+                    if (storage[i] != null) {
+                        System.out.println((i + 1) + ". " + storage[i]);
+                    }
+                }
+            } else {
+                int i = 0;
+                while (storage[i] != null){
+                    i++;
+                }
+                storage[i] = command;
+                System.out.println("added: " + command);
+                
             }
-
-            System.out.println(command);
-            System.out.println("____________________________________________________________");
+            System.out.println("____________________________________________________________\n");
         }
     }
 }
