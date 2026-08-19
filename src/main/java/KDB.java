68b9a3671a1eeb43af6a0e83c94b24c589ad7d77
Laxshan <slytherax11@gmail.com>
2026-08-19 13:25:25 +0000

    Stage 3: Mark as Done (Part 1, no Task Class)

--- src/main/java/KDB.java
+++ src/main/java/KDB.java
@@ -4,14 +4,16 @@ public class KDB {
     public static void main(String[] args) {
 
         String[] storage = new String[100];
+        boolean[] isDone = new boolean[100];
+        int taskCount = 0;
 
         String banner =
-                "mm   mm   mmmmmm    mmmmmmm\n"
-              + "##  ##    ##    ##  ##    ##\n"
-              + "##m##     ##    ##  ##    ##\n"
-              + "#####     ##    ##  #######\n"
-              + "##  ##m   ##    ##  ##    ##\n"
-              + "##   ##m  ##mmm##   ##mmmm##\n";
+        "mm   mm   mmmmmm    mmmmmmm\n"
+      + "##  ##    ##    ##  ##    ##\n"
+      + "##m##     ##    ##  ##    ##\n"
+      + "#####     ##    ##  #######\n"
+      + "##  ##m   ##    ##  ##    ##\n"
+      + "##   ##m  ##mmm##   ##mmmm##\n";
 
         System.out.println("____________________________________________________________");
         System.out.println(banner);
@@ -31,20 +33,39 @@ public class KDB {
                 System.out.println("____________________________________________________________\n");
                 break;
             } else if (command.equals("list")) {
-                for (int i = 0; i < storage.length; i++) {
-                    if (storage[i] != null) {
-                        System.out.println((i + 1) + ". " + storage[i]);
-                    }
+
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    String status = isDone[i] ? "[X]" : "[ ]";
+                    System.out.println((i + 1) + "." + status + " " + storage[i]);
                 }
+
+            } else if (command.startsWith("mark ")) {
+                String[] parts = command.split(" ");
+                int taskNumber = Integer.parseInt(parts[1]);
+                int index = taskNumber - 1;
+
+                isDone[index] = true;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [X] " + storage[index]);
+
+            } else if (command.startsWith("unmark ")) {
+                String[] parts = command.split(" ");
+                int taskNumber = Integer.parseInt(parts[1]);
+                int index = taskNumber - 1;
+
+                isDone[index] = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [ ] " + storage[index]);
+
             } else {
-                int i = 0;
-                while (storage[i] != null){
-                    i++;
-                }
-                storage[i] = command;
+                storage[taskCount] = command;
+                isDone[taskCount] = false;
+                taskCount++;
+
                 System.out.println("added: " + command);
-                
             }
+
             System.out.println("____________________________________________________________\n");
         }
     }
