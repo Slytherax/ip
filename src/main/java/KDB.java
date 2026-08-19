d8af876a1d881921f379f1add2f6a9b946e2ee1b
Laxshan <slytherax11@gmail.com>
2026-08-19 15:30:33 +0000

    STAGE 4: EDIT 2 -> main class updates + touchups

--- src/main/java/KDB.java
+++ src/main/java/KDB.java
@@ -34,8 +34,7 @@ public class KDB {
                 System.out.println("Here are the tasks in your list:");
 
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] "
-                            + tasks[i].getDescription());
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
             } else if (command.startsWith("mark ")) {
                 String[] parts = command.split(" ");
@@ -43,21 +42,48 @@ public class KDB {
 
                 tasks[index].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println("  [" + tasks[index].getStatusIcon() + "] "
-                        + tasks[index].getDescription());
+                System.out.println("  " + tasks[index]);
+
             } else if (command.startsWith("unmark ")) {
                 String[] parts = command.split(" ");
                 int index = Integer.parseInt(parts[1]) - 1;
 
                 tasks[index].markAsNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println("  [" + tasks[index].getStatusIcon() + "] "
-                        + tasks[index].getDescription());
-            } else {
-                tasks[taskCount] = new Task(command);
+                System.out.println("  " + tasks[index]);
+
+            } else if (command.startsWith("todo ")) {
+                tasks[taskCount] = new Todo(command.substring(5));
                 taskCount++;
 
-                System.out.println("added: " + command);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+
+            } else if (command.startsWith("deadline ")) {
+                String[] parts = command.substring(9).split(" /by ", 2);
+
+                tasks[taskCount] = new Deadline(parts[0], parts[1]);
+                taskCount++;
+
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+
+            } else if (command.startsWith("event ")) {
+                String details = command.substring(6);
+                String[] fromParts = details.split(" /from ", 2);
+                String[] toParts = fromParts[1].split(" /to ", 2);
+
+                tasks[taskCount] = new Event(fromParts[0], toParts[0], toParts[1]);
+                taskCount++;
+
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + tasks[taskCount - 1]);
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+
+            } else {
+                System.out.println("I don't understand that command.");
             }
 
             System.out.println("____________________________________________________________");
