676227b88cf05d0a819815568165db5a3c7b9703
Laxshan <slytherax11@gmail.com>
2026-08-19 12:29:44 +0000

    Stage 0: Rename, Greet & Exit

---
+++ src/main/java/KDB.java
@@ -0,0 +1,19 @@
+public class KDB {
+    public static void main(String[] args) {
+        String banner =
+                "mm   mm   mmmmmm    mmmmmmm\n"
+              + "##  ##    ##    ##  ##    ##\n"
+              + "##m##     ##    ##  ##    ##\n"
+              + "#####     ##    ##  #######\n"
+              + "##  ##m   ##    ##  ##    ##\n"
+              + "##   ##m  ##mmm##   ##mmmm##\n";
+
+        System.out.println("____________________________________________________________");
+        System.out.println(banner);
+        System.out.println("Hello! I'm KDB.");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
